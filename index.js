9d3400007d9b005b1bf710381cdd973bff3d36dc
shyamjee19 <shyamjees538@gmail.com>
2025-06-19 12:25:31 +0530

    Update greeting message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, this is my dummy CI/CD project!');
+  res.send('Hello, this is my updated CI/CD project!');
 });
 
 app.listen(3000, () => {
